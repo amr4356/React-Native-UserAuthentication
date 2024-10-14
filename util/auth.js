bed09e52c2f8de80ce0032f33b2ea7244001c337
amr4356 <amrooo73321@gmail.com>
2024-10-14 22:35:04 +0300

    179. Logging Users In

--- util/auth.js
+++ util/auth.js
@@ -2,13 +2,21 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyCuW3QAWakYSDzcrNN0pBgEFN8nrP9K510";
 
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  console.log(response.data);
+}
+
 export async function createUser(email, password) {
-  const resposne = await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+  await authenticate("signUp", email, password);
+}
+
+export async function login(email, password) {
+  await authenticate("signInWithPassword", email, password);
 }
